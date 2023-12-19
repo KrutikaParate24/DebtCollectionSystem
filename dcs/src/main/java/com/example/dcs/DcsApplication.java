1b17966b8512f5487777d3fb73662fc3f8bf600b
KrutikaParate24 <139056720+KrutikaParate24@users.noreply.github.com>
2023-12-19 21:02:56 +0100

    Initial Commit

---
+++ dcs/src/main/java/com/example/dcs/DcsApplication.java
@@ -0,0 +1,13 @@
+package com.example.dcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcsApplication.class, args);
+	}
+
+}
